57476132812bbd8b45c691d4f1aefa51bfc47cfa
jisub <151591300+jisub-dev@users.noreply.github.com>
2025-07-08 11:50:25 +0900

    feat: Section 7. 접근 제어자, <쇼핑카트>연습문제 v1
    feat: Section 7. 접근 제어자, <쇼핑카트>연습문제 v1

---
+++ src/access/ex/ShoppingCart.java
@@ -0,0 +1,29 @@
+package access.ex;
+
+public class ShoppingCart {
+    private Item[] items = new Item[10];
+    private int itemCount;
+
+    public ShoppingCart(){
+        itemCount = 0;
+    }
+
+    public void addItem(Item item){
+        if(itemCount >= 10){
+            System.out.println("장바구니가 가득 찼습니다.");
+            return;
+        }
+        items[itemCount] = item;
+        itemCount++;
+    }
+
+    public void displayItems(){
+        System.out.println("장바구니 상품 출력");
+        int sum = 0;
+        for (int i = 0; i < itemCount; i++) {
+            System.out.println("상품명:" + items[i].getName() + ", 합계:" + items[i].getSum());
+            sum += items[i].getSum();
+        }
+        System.out.println("전체 가격 합:" + sum);
+    }
+}
